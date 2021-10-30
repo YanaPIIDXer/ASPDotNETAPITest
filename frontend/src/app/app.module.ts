3dab3bd09fe1a681efd524a15cf02f494e09e40f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-31 00:07:59 +0900

    ナビゲーションバー用Component定義

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HealthCheckComponent } from './Views/health-check/health-check.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HealthCheckComponent
+    HealthCheckComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
