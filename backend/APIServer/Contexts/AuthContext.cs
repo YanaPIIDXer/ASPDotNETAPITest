e63b203f46b7a9b7212a501e5f9a3f0236be6007
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-30 23:59:54 +0900

    認証関係

---
+++ backend/APIServer/Contexts/AuthContext.cs
@@ -0,0 +1,21 @@
+﻿using APIServer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIServer.Contexts
+{
+	/// <summary>
+	/// 認証関係コンテキスト
+	/// </summary>
+	public class AuthContext : IdentityDbContext<User>
+	{
+		public AuthContext(DbContextOptions options)
+			: base(options)
+		{
+		}
+	}
+}
--- backend/APIServer/Startup.cs
+++ backend/APIServer/Startup.cs
@@ -1,6 +1,10 @@
+using APIServer.Contexts;
+using APIServer.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +36,39 @@ namespace APIServer
                                       builder.WithOrigins("http://localhost")
                                              .AllowCredentials());
             });
+
+            string connStrFormat = "Server={0};Database=APITest;userid=development;password=development";
+            string host = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("IS_DOCKER_CONTAINER")) ? "localhost" : "db";
+            string connStr = string.Format(connStrFormat, host);
+            services.AddDbContext<AuthContext>(option => option.UseMySql(connStr, ServerVersion.AutoDetect(connStr)));
+            services.AddIdentity<User, IdentityRole>()
+                    .AddEntityFrameworkStores<AuthContext>()
+                    .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.AllowedUserNameCharacters = null;
+                options.User.RequireUniqueEmail = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.SlidingExpiration = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +81,7 @@ namespace APIServer
 
             app.UseRouting();
             app.UseCors("AllowOrigins");
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
