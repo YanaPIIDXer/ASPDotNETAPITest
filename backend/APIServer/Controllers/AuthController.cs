df8c17512a6c3eb7905411cb3ce4319d33c318f6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-01 02:12:26 +0900

    ログ表示撤去

--- backend/APIServer/Controllers/AuthController.cs
+++ backend/APIServer/Controllers/AuthController.cs
@@ -12,41 +12,39 @@ using System.Threading.Tasks;
 
 namespace APIServer.Controllers
 {
-	/// <summary>
-	/// 認証関係コントローラ
-	/// </summary>
-	[Route("auth")]
-	[ApiController]
-	public class AuthController : ControllerBase
-	{
-		private UserManager<User> userManager = null;
-		private SignInManager<User> signInManager = null;
+    /// <summary>
+    /// 認証関係コントローラ
+    /// </summary>
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private UserManager<User> userManager = null;
+        private SignInManager<User> signInManager = null;
 
-		public AuthController(UserManager<User> userManager, SignInManager<User> signInManager)
-		{
-			this.userManager = userManager;
-			this.signInManager = signInManager;
-		}
-
-		[Route("register")]
-		[HttpPost]
-		public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
-			Console.WriteLine("UserName:" + request.UserName);
-			Console.WriteLine("Password:" + request.Password);
-			User user = new User();
-			user.UserName = request.UserName;
-			var registerResult = await userManager.CreateAsync(user, request.Password);
-			SimpleResultResponse response = new SimpleResultResponse(registerResult == IdentityResult.Success);
-			return new JsonResult(response);
+            this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
-		[Authorize]
-		public IActionResult Index()
-		{
-			HealthCheckResponse response = new HealthCheckResponse();
-			response.Message = "Auth Health Check OK";
-			return new JsonResult(response);
-		}
-	}
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        {
+            User user = new User();
+            user.UserName = request.UserName;
+            var registerResult = await userManager.CreateAsync(user, request.Password);
+            SimpleResultResponse response = new SimpleResultResponse(registerResult == IdentityResult.Success);
+            return new JsonResult(response);
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            HealthCheckResponse response = new HealthCheckResponse();
+            response.Message = "Auth Health Check OK";
+            return new JsonResult(response);
+        }
+    }
 }
