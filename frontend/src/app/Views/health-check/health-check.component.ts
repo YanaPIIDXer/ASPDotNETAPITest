b06ec580d80f7cf573266ef76d8acc6939e6bb33
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-01 03:44:58 +0900

    ログイン時のヘルスチェック

--- frontend/src/app/Views/health-check/health-check.component.ts
+++ frontend/src/app/Views/health-check/health-check.component.ts
@@ -21,4 +21,16 @@ export class HealthCheckComponent {
     alert(msg);
     console.log(res);
   }
+  
+  async healthCheckWithAuth(): Promise<void> {
+    const res = await conn.get("/auth/");
+    var msg: string = "";
+    if (res.status == 200) {
+      msg = res.json.message;
+    } else {
+      msg = "Fuck.";
+    }
+    alert(msg);
+    console.log(res); 
+  }
 }
