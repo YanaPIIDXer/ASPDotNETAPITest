cc0f016d3081ed9b93e37d666752dc4203c72733
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-02 09:02:54 +0900

    管理者ユーザが作られていなければ作るようにした

--- backend/APIServer/Program.cs
+++ backend/APIServer/Program.cs
@@ -1,5 +1,8 @@
+using APIServer.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,27 @@ namespace APIServer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            createAdmin(host).Wait();
+            host.Run();
+        }
+
+        private static async Task createAdmin(IHost host)
+		{
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var userManager = services.GetService<UserManager<User>>();
+                var adminUser = await userManager.FindByNameAsync("Admin");
+                if (adminUser == null)
+                {
+                    var roleManager = services.GetService<RoleManager<IdentityRole>>();
+                    await roleManager.CreateAsync(new IdentityRole("Admin"));
+                    adminUser = new User() { UserName = "Admin" };
+                    await userManager.CreateAsync(adminUser, "password");
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
